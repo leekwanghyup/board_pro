ab9ab94df0ed4e4c5ce0270967c79e6ed5e6574a
leekwanghyup <leekwanghyup@naver.com>
2022-11-24 17:20:04 +0900

    BoardController : 게시물 목록

--- src/main/java/com/controller/BoardController.java
+++ src/main/java/com/controller/BoardController.java
@@ -1,22 +1,24 @@
 package com.controller;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.dao.BoardDAO;
+import com.service.BoardService;
 
 @WebServlet("/board/*")
 public class BoardController extends HttpServlet {
 	
-	private BoardDAO dao;
+	private BoardService service;
 	
 	@Override
 	public void init() throws ServletException {
-		dao = new BoardDAO();
+		service = new BoardService();
 	}
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
@@ -29,6 +31,19 @@ public class BoardController extends HttpServlet {
 	
 	protected void doHandle(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
+		final String PREFIX = "/WEB-INF/views/board/";
+		final String SUFFIX = ".jsp";
+		
+		String pathInfo = request.getPathInfo(); 
+		String nextPage = null;
+		
+		if(pathInfo.equals("/") || pathInfo.equals("/listArticles") ||  pathInfo==null) {
+			nextPage = "listArticles";
+		} else { // 존재하지 않는 페이지 
+				
+		}
+		RequestDispatcher rd = request.getRequestDispatcher(PREFIX+nextPage+SUFFIX);
+		rd.forward(request, response);
 	}
 
 }
---
+++ src/main/java/com/service/BoardService.java
@@ -0,0 +1,20 @@
+package com.service;
+
+import java.util.List;
+
+import com.dao.BoardDAO;
+import com.domain.ArticleVO;
+
+public class BoardService {
+	
+	BoardDAO dao; 
+	
+	public BoardService() {
+		dao = new BoardDAO(); 
+	}
+	
+	public List<ArticleVO> listArticles() {
+		List<ArticleVO> articlesList = dao.selectAllArticles();
+		return articlesList;
+	}
+}
